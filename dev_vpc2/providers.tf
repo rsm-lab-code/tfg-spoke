819a8bd7f30ba1d29cf149af615502d3b7f68c23
rsm-lab-code@rsmus.com <rsm-lab-code@rsmus.com>
2025-05-19 20:32:16 +0000

    dev_vpc2

---
+++ dev_vpc2/providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+      configuration_aliases = [
+        aws.delegated_account_us-west-2
+      ]
+    }
+  }
+}
---
+++ dev_vpc2/variables.tf
@@ -0,0 +1,37 @@
+variable "aws_regions" {
+  description = "List of AWS regions for deploying resources"
+  type        = list(string)
+  default     = ["us-west-2", "us-east-1"]
+}
+
+variable "delegated_account_id" {
+  description = "AWS Account ID for delegated account where VPC is created"
+  type        = string
+}
+
+variable "ipam_pool_ids" {
+  description = "IDs of subnet pools from IPAM"
+  type        = map(string)
+}
+
+variable "vpc_cidr_netmask" {
+  description = "Netmask for the VPC CIDR allocation"
+  type        = number
+  default     = 21
+}
+
+variable "subnet_prefix" {
+  description = "Additional bits for subnet CIDR division within VPC"
+  type        = number
+  default     = 3
+}
+
+variable "transit_gateway_id" {
+  description = "ID of the Transit Gateway to attach to"
+  type        = string
+}
+
+variable "transit_gateway_route_table_id" {
+  description = "ID of the Transit Gateway route table for workload VPCs"
+  type        = string
+}
