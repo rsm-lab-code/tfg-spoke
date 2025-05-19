efa4220754db7ac2df54fb06d0cf081200fd5a5c
rsm-lab-code@rsmus.com <rsm-lab-code@rsmus.com>
2025-05-19 14:24:41 +0000

    test

--- Dev1_vpc/providers.tf
+++ Dev1_vpc/providers.tf
@@ -1,12 +1,15 @@
-# In modules/vpc/providers.tf
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
       configuration_aliases = [
-        aws.delegated_account_us-west-2,
-        aws.delegated_account_us-east-1
+        aws.delegated_account_us-west-2 
       ]
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
     }
   }
 }
